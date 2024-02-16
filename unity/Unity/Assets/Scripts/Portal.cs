a03c65b225b7eb7448a7dcdfa2828a8c289e3114
Noelia Barranco Godoy <56674140+NotNoe@users.noreply.github.com>
2024-02-16 20:42:36 +0100

    Portales empezados

---
+++ unity/Unity/Assets/Scripts/Portal.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Portal : MonoBehaviour
+{
+    #region references
+        [SerializeField] GameObject linked_portal;
+    #endregion
+    #region parameters
+
+    #endregion
+    #region properties
+
+    #endregion
+    #region methods
+        public void set_link(){
+
+        }
+    #endregion
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
